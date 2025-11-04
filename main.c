89f22bf8d39b48531a9ca35aa5ea2ab76885e2c0
Radheshyam Routh <radheshyamrouth001@gmail.com>
2025-11-04 20:06:31 +0530

    Lagrange interpolation formula

--- main.c
+++ main.c
@@ -1,68 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-                  // The following programming taking only the value of x and gives f(x) using Newton's forward interpolation method from a given data set
-                  // f(x) = p(x)= y_0 + (u/1!){del}y_0 + (u(u-1)/2!){del^2}y_0+.....+(u(u-1)..(u-i+1)/n!){del^n}y_0
+     // The following programming approximate the vale of x[j] (user given) from a predefined data set by Lagrange interpolation formula.
+     // p(x[j])= L[0](x[j])*y[0]+L[1](x[j])*y[1]+...+L[n](x[j])*y[n] ...here no of data points is (n+1)
+     //where L[i](x[j])={(x[j]-x[0])(x[j]-x[1])...(x[j]-x[i-1])(x[j]-x[i+1])...(x[j]-x[n])}/{(x[i]-x[0])(x[i]-x[1])...(x[i]-x[i-1])(x[i]-x[i+1])...(x[i]-x[n])}
+
 int main()
 {
-    int n,i, j ;
-    float   h, u,x_0, p, c, y_0, k, l, fact, t;
-
+    int    n,i, j ;
+    float  u, p, c, k;
     printf("Enter your value for x: ");                          // here given x=0.05; but I am taking here the value x from user
-    scanf("%f", &c);                                             // here c is working as x because the programming fails to recognize different different x
-
-
+    scanf("%f", &c);                                             // here c is working as x or x[j] because the programming fails to recognize different different x
     float x[5]={0.0, 0.2, 0.4, 0.6, 0.8};                          // no of data points denote here so x[5]. one can write it as float x[4]; then x[]={data points}.
-    float dy[5][5]={1.00000, 1.22140, 1.49182, 1.82212, 2.22554};  // these values are stored in the 1st row of 5*5 matrix which is dy[0][i]
+    float y[5]={1.00000, 1.22140, 1.49182, 1.82212, 2.22554};
 
     n=4;
-
-
-    y_0=dy[0][0];
-
-
-    for(j=1; j<=n; j++)
+    u=0;
+    for(i=0; i<=n; i++)
     {
-        for(i=0; i<=n-j; i++)
+        p=1;
+        for(j=0; j<=n; j++)
         {
-            dy[j][i]=dy[j-1][i+1]- dy[j-1][i];                       // calculating del
-                                                                     printf("dy[%d][%d] is %f \n", j, i,dy[j][i]);
+           if(j!=i)
+            {
+                //printf("i = %d, j =%d\n",i,j);
+                p=p*(c-x[j])/(x[i]-x[j]);                           //calculating L[i]=p
+                //printf("for i= %d and j=%d p is %f \n", i,j,p);
+                }
         }
-    };
-
-    h=x[1]-x[0];                                                   // here the difference h are same h=x[2]-x[1]
-
-    u=(c-x[0])/h;                                              // u=(x-x_0)/h
-                                                               // printf("u is %f \n \n",u);
-
-                                                                // p(x)= y_0+ k;
-
-   k=0;
-   l=1;
-   for(j=1; j<=n; j++)             //calculating the numerator and denominator from 1st term
-   {
-       fact=1;
-       for(i=1; i<=j; i++)
-       {
-          fact=fact*i;                                 //calculating i!
-
-        }
-                                                     //  printf("fact of %d is %f \n", j,fact);
-
-       l=l*(u-j+1);                                  // l stores the value of u*(u-1)*..*(u-i+1)
-
-                                                    // printf("value of l is %f for %d \n", l, j);
-
-       k=k+(l*dy[j][0])/fact;
-
-                                                    // printf("the value of k is %f for %d \n", k,j);
-
-   }
-
-
-   p=dy[0][0]+k;
-   printf("p(x) is %f ", p);
-
-
-
+        u=u+y[i]*p;                                                 // multiplication of L[i]*y[i] and calculating p(x[j]) or p(x)=u i.e. the final value
+    }
+    printf("p(x) is %f", u);
 }
